3e8883cd70b88541568c849e9629f8a7293acf8b
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-11-29 21:03:13 +0100

    add constructor

--- src/Exception/DatabaseException.php
+++ src/Exception/DatabaseException.php
@@ -3,8 +3,12 @@
 namespace PierreMiniggio\DatabaseConnection\Exception;
 
 use Exception;
+use Throwable;
 
-abstract class DatabaseException extends Exception
+class DatabaseException extends Exception
 {
-    
+    public function __construct(string $message, int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
